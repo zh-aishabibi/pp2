cc7aaeb4d2b49522f24990d8e2d91a9d633bbce5
zh-aishabibi <ais_zhaksylyk@kbtu.kz>
2025-01-31 04:58:59 +0600

    commit

---
+++ week2/bitwise_op.py
@@ -0,0 +1,7 @@
+x = 52 
+y = 6
+
+print (x&y) #if two 1 then 1
+print (x^y) #only one 1
+print (x|y) #at least one 1
+print (~x) #inverts all the bit 
---
+++ week2/bool.py
@@ -0,0 +1,11 @@
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
